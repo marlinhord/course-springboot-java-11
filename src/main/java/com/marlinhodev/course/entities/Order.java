10a613b518252f2235fac761ceeb315dfdca35f0
Marlon Araujo <marloncavalcanti1@gmail.com>
2022-04-11 22:19:07 -0300

    Payment, one-to-one association

--- src/main/java/com/marlinhodev/course/entities/Order.java
+++ src/main/java/com/marlinhodev/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -39,8 +41,10 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
-		
 	}
 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -67,8 +71,6 @@ public class Order implements Serializable{
 		this.moment = moment;
 	}
 	
-	
-
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus) ;
 	}
@@ -85,6 +87,14 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		Client = client;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+	
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 	
 	public Set<OrderItem> getItems(){
